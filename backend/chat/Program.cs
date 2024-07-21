538f3d6cd74c26a6c616ea7ca06d0ae740dc7aa9
Anton Anton <iamanton45@gmail.com>
2024-07-21 17:40:42 +0100

    preparation for working with docker

--- backend/chat/Program.cs
+++ backend/chat/Program.cs
@@ -8,20 +8,28 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = connection;
 });
 
-builder.Services.AddCors(options =>
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(config =>
 {
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.WithOrigins("http://localhost:3000")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
-    });
+    config.AddPolicy("DefaultPolicy",
+        builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 });
 
 builder.Services.AddSignalR();
 
 var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.MapHub<ChatHub>("/chat");
 
